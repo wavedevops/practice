e5ce20d76958205c203971f5e6fd157c841b845c
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-18 20:29:47 +0530

    automated commit #3343

--- shell/desk-space.sh
+++ shell/desk-space.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DISK_SPACE=$( df -hT | grep xfs )
+DISK_SPACE=$(df -hT | grep xfs)
 DISK_THRESHOL="80"
 
 while IFS= read -r line
 do
-  USAGE=$( echo $line )
+  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d '%' -f1) 
 
 done <<< $DISK_SPACE
