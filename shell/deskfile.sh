60e7d0d36b02f76d3adcf09f99c75d0e27cee2e0
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-18 19:39:46 +0530

    automated commit #3329

---
+++ shell/deskfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+N="\e[0m"
+R="\e[31m]"
+G="\e[32m"
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+if[ -d $SOURCE_DIRECTORY ]
+tehn
+    echo "$G source directory already exists $N"
+else
+    echo "$R $SOURCE_DIRECTORY desnot ecits $N"
+fi
