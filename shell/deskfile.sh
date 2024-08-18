3b5ebc9d1907d383307d880dcc0cd1704d4bdbc6
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-18 19:47:49 +0530

    automated commit #3333

--- shell/deskfile.sh
+++ shell/deskfile.sh
@@ -6,9 +6,24 @@ G="\e[32m"
 
 SOURCE_DIRECTORY=/tmp/app-logs
 
-if[ -d $SOURCE_DIRECTORY ]
- then
+if[ -d $SOURCE_DIRECTORY ];
+then
      echo -e "$G source directory already exists $N"
- else
+else
      echo -e "$R $SOURCE_DIRECTORY desnot ecits $N"
+fi
+
+#!/bin/bash
+
+N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+
+SOURCE_DIRECTORY="/tmp/app-logs"
+
+if [ -d "$SOURCE_DIRECTORY" ]; 
+then
+    echo -e "${G}Source directory already exists${N}"
+else
+    echo -e "${R}${SOURCE_DIRECTORY} does not exist${N}"
 fi
