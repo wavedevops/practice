3dfefe9a011040bfea47680fead52286662969f9
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-19 07:43:52 +0530

    automated commit #3344

--- shell/desk-space.sh
+++ shell/desk-space.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 DISK_SPACE=$(df -hT | grep xfs)
-DISK_THRESHOL="80"
+DISK_THRESHOL="10"
 
 while IFS= read -r line
 do
-  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d '%' -f1) 
-
+   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d '%' -f1) 
+   FOLDER=$(echo $line | awk -F " " '{print $NF}')
+   if [ $USAGE -ge $DISK_THRESHOL ]
+   then
+       echo " $FOLDER is more than $DISK_THRESHOL , current usage is $USAGE"
+   fi
 done <<< $DISK_SPACE
