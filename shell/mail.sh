a5eef5f2d7d8ffdb129bfee45230a06d8a2d3d8f
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-19 10:04:30 +0530

    automated commit #3351

---
+++ shell/mail.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+N='\e[0m'
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+
+DISK_SPACE=$(df -hT | grep xfs)
+DISK_THRESHOL="70"
+MESSAGE='' # empty variable
+
+while IFS= read -r line
+do
+   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d '%' -f1) 
+   FOLDER=$(echo $line | awk -F " " '{print $NF}')
+   if [ $USAGE -ge $DISK_THRESHOL ]
+   then
+       MESSAGE+=" $Y $FOLDER $N is more than $G $DISK_THRESHOL $N, current usage is :$R $USAGE $N \n" 
+       # += prives value cant overide and new value adding 
+       # \n men new line
+   fi
+done <<< $DISK_SPACE
+
+echo "message: $MESSAGE"
