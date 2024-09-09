0fc3d9e5eda2db1fdb268482bd2a18ea79c0adad
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-09 18:08:28 +0530

    automated commit #15

--- vpc/terraform.tf
+++ vpc/terraform.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "chowdary-hari"
-    key    = "test/development/terraform.state"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "chowdary-hari"
+#     key    = "test/development/terraform.state"
+#     region = "us-east-1"
+#   }
+# }
