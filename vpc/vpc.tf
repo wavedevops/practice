e9c8444bbcfd983971ef77aa38fa1412a66f250b
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-11 18:18:25 +0530

    automated commit #43

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,5 +1,18 @@
+# module "vpc" {
+#   source = "../terraform"
+#   component = var.component
+#   env = var.env
+#   vpc_cidr = var.vpc_cidr
+#   common_tags = var.common_tags
+#   azs = var.azs
+#   private_subnets = var.private_subnets
+#   public_subnets = var.public_subnets
+#   database_subnets = var.database_subnets
+#   default_vpc_id = var.default_vpc_id
+#   default_vpc_cidr = var.default_vpc_cidr
+# }
 module "vpc" {
-  source = "../terraform"
+  source =   "git::https://github.com/Chowdary-Hari/terraform.git"
   component = var.component
   env = var.env
   vpc_cidr = var.vpc_cidr
