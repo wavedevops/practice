891a193b895899f8125c677e3e21bf2d20006ba0
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-11 13:20:25 +0530

    automated commit #38

--- terraform/main.tf
+++ terraform/main.tf
@@ -125,3 +125,9 @@ resource "aws_route_table" "database" {
     }
   )
 }
+
+resource "aws_route" "public_route" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
