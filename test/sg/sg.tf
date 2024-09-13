76d21417f9260b11301ac5c14d3a707d15c7cafa
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-13 20:13:15 +0530

    automated commit #55

--- test/sg/sg.tf
+++ test/sg/sg.tf
@@ -44,179 +44,6 @@ module "ansible" {
 }
 
 
-# ## db
-# resource "aws_security_group_rule" "database" {
-#   type              = "ingress"
-#   from_port         = 3306
-#   to_port           = 3306
-#   protocol          = "tcp"
-#   source_security_group_id = module.backend.sg_id  # allow all cidr's  from backend sg
-#   security_group_id = module.database.sg_id
-# }
-# resource "aws_security_group_rule" "database_bastion" {
-#   type              = "ingress"
-#   from_port         = 3306
-#   to_port           = 3306
-#   protocol          = "tcp"
-#   source_security_group_id = module.bastion.sg_id  # allow all cidr's  from backend sg
-#   security_group_id = module.database.sg_id
-# }
-#
-#
-#
-#
-#
-#
-#
-# resource "aws_security_group_rule" "backend" {
-#   type              = "ingress"
-#   from_port         = 8080
-#   to_port           = 8080
-#   protocol          = "tcp"
-#   source_security_group_id = module.frontend.sg_id
-#   security_group_id = module.backend.sg_id
-# }
-#
-# resource "aws_security_group_rule" "backend_bastion" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   source_security_group_id = module.bastion.sg_id
-#   security_group_id = module.backend.sg_id
-# }
-#
-#
-#
-#
-#
-# resource "aws_security_group_rule" "frontend" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = module.frontend.sg_id
-# }
-#
-# resource "aws_security_group_rule" "frontend_bastion" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   source_security_group_id = module.bastion.sg_id
-#   security_group_id = module.frontend.sg_id
-# }
-#
-# resource "aws_security_group_rule" "frontend_ansible" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   source_security_group_id = module.ansible.sg_id
-#   security_group_id = module.frontend.sg_id
-# }
-#
-#
-#
-#
-#
-# resource "aws_security_group_rule" "public_bastion" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = module.bastion.sg_id
-# }
-#
-# resource "aws_security_group_rule" "ansible_public" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = module.ansible.sg_id
-# }
-
-
-
-######################
-
-
-# DB is accepting connections from backend
-resource "aws_security_group_rule" "db_backend" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  source_security_group_id = module.backend.sg_id # source is where you are getting traffic from
-  security_group_id = module.database.sg_id
-}
-
-resource "aws_security_group_rule" "db_bastion" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
-  security_group_id = module.database.sg_id
-}
-
-resource "aws_security_group_rule" "backend_frontend" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  source_security_group_id = module.frontend.sg_id # source is where you are getting traffic from
-  security_group_id = module.backend.sg_id
-}
-
-resource "aws_security_group_rule" "backend_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
-  security_group_id = module.backend.sg_id
-}
-
-resource "aws_security_group_rule" "backend_ansible" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id = module.ansible.sg_id # source is where you are getting traffic from
-  security_group_id = module.backend.sg_id
-}
-
-resource "aws_security_group_rule" "frontend_public" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = module.frontend.sg_id
-}
-
-resource "aws_security_group_rule" "frontend_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
-  security_group_id = module.frontend.sg_id
-}
-
-resource "aws_security_group_rule" "frontend_ansible" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id = module.ansible.sg_id # source is where you are getting traffic from
-  security_group_id = module.frontend.sg_id
-}
-
 resource "aws_security_group_rule" "bastion_public" {
   type              = "ingress"
   from_port         = 22
@@ -235,4 +62,81 @@ resource "aws_security_group_rule" "ansible_public" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.ansible.sg_id
 }
+
+
+# DB
+resource "aws_security_group_rule" "database" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.backend.sg_id
+  security_group_id = module.database.sg_id
+}
+
+resource "aws_security_group_rule" "db_bastion" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.database.sg_id
+}
+
+# backend
+resource "aws_security_group_rule" "backend" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.frontend.sg_id
+  security_group_id = module.backend.sg_id
+}
+
+resource "aws_security_group_rule" "backend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.backend.sg_id
+}
+
+resource "aws_security_group_rule" "backend_ansible" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.ansible.sg_id
+  security_group_id = module.backend.sg_id
+}
+
+# frontend
+resource "aws_security_group_rule" "frontend" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.frontend.sg_id
+}
+
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.frontend.sg_id
+}
+
+resource "aws_security_group_rule" "frontend_ansible" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.ansible.sg_id
+  security_group_id = module.frontend.sg_id
+}
+
 
