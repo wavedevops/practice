0a1203d3a16e8726c089fa28379cf3e43fc687f7
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-15 14:33:37 +0530

    automated commit #64

--- test/sg/perrametter.tf
+++ test/sg/perrametter.tf
@@ -1,17 +1,29 @@
-resource "aws_ssm_parameter" "sg_id" {
-  name  = "/${var.project}/${var.env}/database"
+resource "aws_ssm_parameter" "database" {
+  name  = "/${var.project}/${var.env}/database_sg_id"
   type  = "String"
   value = module.database.sg_id
 }
 
-resource "aws_ssm_parameter" "sg_id" {
-  name  = "/${var.project}/${var.env}/backend"
+resource "aws_ssm_parameter" "backend" {
+  name  = "/${var.project}/${var.env}/backend_sg_id"
   type  = "String"
   value = module.backend.sg_id
 }
 
-resource "aws_ssm_parameter" "sg_id" {
-  name  = "/${var.project}/${var.env}/frontend"
+resource "aws_ssm_parameter" "frontend" {
+  name  = "/${var.project}/${var.env}/frontend_sg_id"
   type  = "String"
   value = module.frontend.sg_id
+}
+
+resource "aws_ssm_parameter" "ansible" {
+  name  = "/${var.project}/${var.env}/ansible_sg_id"
+  type  = "String"
+  value = module.ansible.sg_id
+}
+
+resource "aws_ssm_parameter" "bastion" {
+  name  = "/${var.project}/${var.env}/bastion_sg_id"
+  type  = "String"
+  value = module.bastion.sg_id
 }
