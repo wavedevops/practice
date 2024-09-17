827f460f127f0cd3f791ece2e6978315daf041ce
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-17 16:44:55 +0530

    automated commit #67

--- terraform/04.rds/main.tf
+++ terraform/04.rds/main.tf
@@ -1,22 +1,64 @@
-resource "aws_rds_cluster" "main" {
-  cluster_identifier     = "${var.env}-${var.component}"
-  engine                 = "aurora-mysql"
-  engine_version         = "5.7.mysql_aurora.2.11.3"
-  db_subnet_group_name   = data.aws_ssm_parameter.db_subnet_id.value
-  database_name          = "tranctions"
-  master_username        = "root"
-  master_password        = "ExpenseApp1"
-  vpc_security_group_ids = [data.aws_ssm_parameter.database_sg_id.value]
-  skip_final_snapshot    = true
-#   kms_key_id             = var.kms_key_id
-  storage_encrypted      = true
-}
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+  identifier = "${var.component}-${var.env}" #expense-dev
 
-resource "aws_rds_cluster_instance" "main" {
-  count              = 1
-  identifier         = "${var.env}-${var.component}-instance-${count.index}"
-  cluster_identifier = aws_rds_cluster.main.id
-  instance_class     = "db.t3.medium"
-  engine             = aws_rds_cluster.main.engine
-  engine_version     = aws_rds_cluster.main.engine_version
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 5
+
+  db_name  = "transactions" #default schema for expense project
+  username = "root"
+  port     = "3306"
+
+  vpc_security_group_ids = [data.aws_ssm_parameter.database_sg_id.value]
+
+  # DB subnet group
+  db_subnet_group_name = data.aws_ssm_parameter.db_subnet_id.value
+
+  # DB parameter group
+  family = "mysql8.0"
+
+  # DB option group
+  major_engine_version = "8.0"
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.component}-${var.env}"
+    }
+  )
+
+  manage_master_user_password = false
+  password = "ExpenseApp1"
+  skip_final_snapshot = true
+
+  parameters = [
+    {
+      name  = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name  = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+
 }
--- terraform/04.rds/vars.tf
+++ terraform/04.rds/vars.tf
@@ -1,9 +1,21 @@
 variable "env" {
   default = "dev"
 }
+
 variable "component" {
   default = "mysql"
 }
+
 variable "project" {
   default = "expense"
+}
+
+
+variable "common_tags" {
+  default =
+  {
+    Project     = "expense"
+    Environment = "dev"
+    Terrafom    = "teue"
+  }
 }
