d8e6e27e07b2ee11eb663544f8c65eca78295561
Chowdary-Hari <chandrahari321@outlook.com>
2026-06-23 11:18:57 +0530

    update

--- shell/commnds.sh
+++ shell/commnds.sh
@@ -3,17 +3,32 @@
 status_check() {
     if [ $? -eq 0 ]; then
         echo -e "\e[32mSUCCESS\e[0m"
-        sleep 5
+        sleep 2
     else
         echo -e "\e[31mFAILURE\e[0m"
         exit 1
     fi
 }
-
+echo Adding a User hari
 sudo useradd hari
 status_check
 
+echo Adding a Grroup
+sudo groupadd devops
+status_check
 
+echo create a file 
+sudo touch file.txt
+status_check
 
-##
+echo Assining user to group
+sudo usermod -aG devops hari
+status_check
 
+echo Change oner ship of file 
+sudo chmod hari file.txt 
+status_check
+
+echo "Chande group of file"
+sudo grpmod devops file.txt
+status_check
