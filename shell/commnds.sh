c187dfea3d38d514ad84f883f4f9a4cb30840ee1
Chowdary-Hari <chandrahari321@outlook.com>
2026-06-23 11:00:47 +0530

    update

---
+++ shell/commnds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+status_check() {
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mSUCCESS\e[0m"
+        sleep 5
+    else
+        echo -e "\e[31mFAILURE\e[0m"
+        exit 1
+    fi
+}
+
+useradd hari
+status_check
+
+
+
+##
+
