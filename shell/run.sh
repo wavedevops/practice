27d30615f112c278341be4c259aec15151acfdea
Chowdary-Hari <chandrahari321@outlook.com>
2026-06-27 11:28:46 +0530

    automated commit #42

--- shell/run.sh
+++ shell/run.sh
@@ -6,4 +6,4 @@ echo -e "\e[32mplease enter your age :\e[0m"
 read age
 echo -e "\e[32mplease enter your cource :\e[0m"
 read cource
-echo -e "\e[33mname is $name , age is $age , cource is $cource.\e[0m"
+echo -e "\e[33mname is '$name' , age is '$age' , cource is '$cource'.\e[0m"
