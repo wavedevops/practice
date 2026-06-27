793c2b84023c1ffbe16fe5d1a71964aa022b62f3
Chowdary-Hari <chandrahari321@outlook.com>
2026-06-27 08:53:36 +0530

    automated commit #39

--- shell/run.sh
+++ shell/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "\e[32mplease enter your name :\e[0m" 
+echo -e "\e[32mplease enter your name :\e[0m"
 read name
-echo -e "\e[33mhay $name how are you\e[0m"
+echo -e "\e[33mhey $name how are you\e[0m"
