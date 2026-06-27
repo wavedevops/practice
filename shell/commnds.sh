75f654271b56d55c6abe78d9aac9100c86acda99
Chowdary-Hari <chandrahari321@outlook.com>
2026-06-27 06:36:38 +0530

    Automated commit #15

--- shell/commnds.sh
+++ shell/commnds.sh
@@ -1,27 +1,2 @@
 #!/bin/bash
 
-status_check() {
-    if [ $? -eq 0 ]; then
-        echo -e "\e[32mSUCCESS\e[0m"
-        sleep 2
-    else
-        echo -e "\e[31mFAILURE\e[0m"
-    fi
-}
-
-read name 
-
-echo "present working directory"
-pwd
-status_check
-
-
-echo "varible form other file"
-echo "my name is $name"
-status_check
-
-echo "commnd substute"
-echo "to day date is $(date)" 
-status_check
-
-
