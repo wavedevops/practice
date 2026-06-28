ae4041201ce38bcfd0ea8261dddb5a96bf93497c
Chowdary-Hari <chandrahari321@outlook.com>
2026-06-28 16:20:25 +0530

    automated commit #49

--- shell/run.sh
+++ shell/run.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-echo -e "\e[32mplease enter your name :\e[0m"
-read name
-echo -e "\e[32mplease enter your age :\e[0m"
-read age
-echo -e "\e[32mplease enter your cource :\e[0m"
-read course
-echo -e "\e[33mname is '$name' , age is '$age' , cource is '$course'.\e[0m"
+i=1
 
-if [[ "$course" == "DevOps" ]]; then
-    echo "This student is eligible for DevOps."
-elif [[ "$course" == "Python" ]]; then
-    echo "This student is eligible for Python."
-else
-    echo "This student is not eligible for my institute."
-fi
-
-for i in $(seq 1 9); do $i ; done  
-for i in ; do $i ; done 
+while [ $i -le 3 ]
+do
+    echo "$i"
+    ((i++))
+done
