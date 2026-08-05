079dd55305e33a24d14d2f440d91ad500d1a2d78
Chowdary-Hari <chandrahari321@outlook.com>
2026-08-05 19:05:46 +0530

    automated commit #221

---
+++ terraform/test.tf
@@ -0,0 +1,27 @@
+resource "null_resource" "main" {
+
+  connection {
+    type     = "ssh"
+    host     = "172.31.35.13"
+    user     = "ec2-user"
+    password = "DevOps321"
+    # Better practice:
+    # private_key = file("~/.ssh/id_rsa")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-pull -i localhost, -U https://github.com/wavedevops/expense-ansible.git -e env=${var.env} expense.yml --tags=${var.component}"
+    ]
+  }
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "component" {
+  type    = string
+  default = "frontend"
+}
